4532d739aec72fc0fceddf0629b6a9800d14b2c9
Aliona Butkevich <a.butckevich@gmail.com>
2017-09-16 14:13:18 +0300

    put logo into a single component

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Logo from '../logo/logo';
 import './footer.scss';
 
 const Footer = () => (
-    <div className="footer">netflixroulette</div>
+    <div className="footer">
+        <Logo></Logo>
+    </div>
 )
 
 export default Footer;
---
+++ src/logo/logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './logo.scss';
+
+const Logo = () => (
+    <div className="logo">netflixroulette</div>
+)
+
+export default Logo;
