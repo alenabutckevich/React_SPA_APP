a78b1957329d8a23257fc6d4373bf146ce2e1c0c
Aliona Butkevich <a.butckevich@gmail.com>
2017-10-10 22:39:14 +0300

    created store

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './app';
 import Main from './pages/main';
 import MovieDetails from './pages/movie-details';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Router>
-        <App>
-            <Route exact path="/" component={Main} />
-            <Route path="/search/:query" component={Main} />
-            <Route path="/film/:title" component={MovieDetails} />
-        </App>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App>
+                <Route exact path="/" component={Main} />
+                <Route path="/search/:query" component={Main} />
+                <Route path="/film/:title" component={MovieDetails} />
+            </App>
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
