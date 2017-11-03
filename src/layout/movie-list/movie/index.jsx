770af9465c3ee5eba78a840f4b286678250b7203
Aliona Butkevich <a.butckevich@gmail.com>
2017-11-03 23:21:55 +0300

    added test for Movie component

--- src/layout/movie-list/movie/index.jsx
+++ src/layout/movie-list/movie/index.jsx
@@ -6,7 +6,7 @@ const Movie = ({id, image, title, releaseDate, genre}) => (
     <div className="movie">
         <Link to={`/film/${id}`}><img className="movie__image" src={`https://image.tmdb.org/t/p/w500/${image}`}/></Link>
         <span className="movie__title">{title}</span>
-        <input className="movie__release-date" value={releaseDate.split('-')[0]} readOnly/>
+        <input className="movie__release-date" value={releaseDate.split('-')[2]} readOnly/>
         <div style={{clear: 'both'}}/>
         <span className="movie__genre">{genre}</span>
     </div>
