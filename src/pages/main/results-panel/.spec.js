f17d0b7dd8b4aadda6571bb2d2f337d8838b55d2
Aliona Butkevich <a.butckevich@gmail.com>
2017-11-06 23:38:05 +0300

    (results-panel): added component tests

---
+++ src/pages/main/results-panel/.spec.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import ResultsPanel from './results-panel';
+
+const setup = propOverrides => {
+    const { movies } = propOverrides;
+    const props = Object.assign({
+        movies: movies
+    }, propOverrides);
+
+    const wrapper = shallow(<ResultsPanel {...props}/>);
+
+    return {
+        wrapper,
+        props
+    }
+}
+
+describe('<ResultsPanel />', () => {
+
+    it('render results panel', () => {
+        const movies = [{
+            id:1, title:'title1', poster_path:'path1', genre_ids:[1], release_date:'12-12-2012'
+        }, {
+            id:2, title:'title2', poster_path:'path2', genre_ids:[2], release_date:'12-12-2012'
+        }];
+        const { wrapper } = setup({movies: movies});
+        expect(wrapper.find('.results-panel__movies-count').text()).toEqual('2 movies found');
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    })
+
+    it('no render results panel', () => {
+        const { wrapper } = setup([]);
+        expect(wrapper.find('.results-panel__movies-count')).toHaveLength(0);
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    })
+})
