eab87c55f3e6fb9f970a4eb862518f8c873098d4
Aliona Butkevich <a.butckevich@gmail.com>
2017-11-12 17:32:51 +0300

    added server rendering

--- src/client/sagas/index.js
+++ src/client/sagas/index.js
@@ -1,18 +1,13 @@
-import { fork } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects';
 import { watchFetchGenres } from './genreList';
 import { watchFetchMovieById, watchFetchMovieCastAndCrew } from './movie';
 import { watchFetchMovies } from './movieList';
 
 export default function* rootSaga() {
-  // yield fork ([
-  //   watchFetchGenres,
-  //   watchFetchMovieById,
-  //   watchFetchMovieCastAndCrew,
-  //   watchFetchMoviesByTitle,
-  //   watchFetchMoviesByDirector
-  // ]);
-  yield fork(watchFetchGenres);
-  yield fork(watchFetchMovieById);
-  yield fork(watchFetchMovieCastAndCrew);
-  yield fork(watchFetchMovies);
+  yield all([
+    watchFetchGenres(),
+    watchFetchMovieById(),
+    watchFetchMovieCastAndCrew(),
+    watchFetchMovies()
+  ])
 }
